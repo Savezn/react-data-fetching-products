93c07bf8f1e54465250de446c3ed79e5a17ebb7d
Charlee2543 <165679195+Charlee2543@users.noreply.github.com>
2025-03-25 16:10:44 +0700

    refactor:remove post product

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,48 @@
 import './App.css';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-
+// เอาไอดีมาใส่หลังAPI
 function App() {
     // เมื่อมีการ rerender
     useEffect(() => {
-        resilt();
+        result();
     }, []);
+
     // ดึงข้อมูลจากserverเข้ามา
-    const resilt = async () => {
+    const result = async () => {
         const produst = await axios.get('http://localhost:4001/products');
         setData(produst.data.data);
         // console.log(produst.data.data);
     };
-    // เก็บข้อมูลจาก server
-    const [data, setData] = useState();
 
+    // เก็บข้อมูลจาก server
+    const [data, setData] = useState([]);
+    // const [dataDelete, setDataDelete] = useState();
+    console.log('data', data);
+
+    // execute delete data
+    const removeData = async (event) => {
+        const idDelet = Number(event.target.value);
+        // ให้ลบข้อมูลใน server
+        // console.log(event.target.value);
+        // console.log(typeof idDelet);
+
+        await axios.delete(`http://localhost:4001/products/${idDelet}`);
+        // result();
+        // ให้ลบข้อมูลใน UseState data
+        const deleteData = data.filter((product) => {
+            console.log('product.idDelet', idDelet);
+            console.log('product.id ', product.id);
+            return idDelet !== product.id;
+        });
+        console.log(deleteData);
+
+        setData(deleteData);
+    };
     // console.log(data);
 
     return data.map((productData) => (
-        <div className="App">
+        <div className="App" key={productData.id}>
             <div className="app-wrapper">
                 <h1 className="app-title">Products</h1>
             </div>
@@ -39,7 +62,12 @@ function App() {
                         <p>Product description: {productData.description}</p>
                     </div>
 
-                    <button className="delete-button" value={productData.id}>
+                    <button
+                        className="delete-button"
+                        value={productData.id}
+                        // onClick={() => removeData(productData.id)}
+                        onClick={removeData}
+                    >
                         x
                     </button>
                 </div>
