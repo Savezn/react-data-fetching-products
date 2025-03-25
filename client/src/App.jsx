b1677697e09c082c539422c7d8752e681839a2e0
Your Name <you@example.com>
2025-03-25 15:43:41 +0700

    fetch and display product from sever, add delete functionnality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,57 @@
 import "./App.css";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const main = async () => {
+      const responese = await axios.get("http://localhost:4001/products");
+      setProducts(responese.data.data || []);
+    };
+    main();
+  }, []);
+  console.log(products);
+  
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:4001/products/${id}`);
+      // อัปเดต State โดยกรองสินค้าที่ถูกลบออก
+      setProducts(products.filter((product) => product.id !== id));
+    } catch (error) {
+      console.error("Error deleting product:", error);
+    }
+  };
+
   return (
     <div className="App">
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
-      <div className="product-list">
+
+      {products.map((item) => 
+      <div className="product-list" key={item.id}>
         <div className="product">
           <div className="product-preview">
             <img
-              src="https://via.placeholder.com/350/350"
-              alt="some product"
+              src={item.image}
+              alt={item.name}
               width="350"
               height="350"
             />
           </div>
           <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
+            <h1>Product name: {item.name}</h1>
+            <h2>Product price: {item.price}</h2>
+            <p>Product description: {item.description}</p>
           </div>
 
-          <button className="delete-button">x</button>
+          <button className="delete-button" onClick={() => handleDelete(item.id)}>x</button>
         </div>
       </div>
+      )}
     </div>
   );
 }
